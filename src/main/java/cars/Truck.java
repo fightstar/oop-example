086795aa15dd94f9dbdcd6543b0d7c4970a1b359
fightstar <vladislav.shevchenko@gmail.com>
2014-02-17 22:33:04 +0200

    add classes

---
+++ src/main/java/cars/Truck.java
@@ -0,0 +1,11 @@
+package cars;
+
+import com.luxoft.oop.Car;
+
+/**
+ * Created by Vlad on 17.02.14.
+ */
+public class Truck extends Car {         // Это наследование
+    Car truck = new Car(90,120,4578,"Maz");
+
+}
---
+++ src/main/java/com/luxoft/oop/IVehicle.java
@@ -0,0 +1,13 @@
+package com.luxoft.oop;
+
+/**
+ * Created by Vlad on 17.02.14.
+ */
+public interface IVehicle {
+
+    public void setSpeed(int speed);
+    public void setCapacity(int capacity);
+    public void setNumber(int number);
+    public void drive();
+
+}
